8c52655502aa302ab6a3630e017a458fcf13886b
SValenca <gabrielsoaresvalenca@gmail.com>
2024-10-31 00:18:13 -0300

    correçao

--- js/main.js
+++ js/main.js
@@ -2,18 +2,48 @@
 navBarComponent()
 
 function navBarComponent() {
+
     const element = document.getElementById(`navbar-component`)
     const navbar = `
-     <nav class="navbar">
-    <ul class="nav-links">
-        <li><a href="index.html">Início</a></li>
-        <li><a href="index.html#services">Serviços</a></li>
-        <li><a href="index.html#about">Sobre Nós</a></li>
-        <li><a href="index.html#contact">Contato</a></li>
-        <li><a href="login.html">Entrar</a></li>
+    <nav class="navbar navbar-expand-lg ">
+    <div class="container fluid">
+    <!-- <a class="navbar-brand" href="#">Navbar</a> -->
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav">
+    <li class="nav-item">
+    <a class="nav-link" href="index.html">Início</a>
+    </li>
+    <li class="nav-item">
+    <a class="nav-link" href="#services">Serviços</a>
+    </li>
+    <li class="nav-item">
+    <a class="nav-link" href="#about">Sobre Nós</a>
+    </li>
+    <li class="nav-item">
+    <a class="nav-link" href="#contact">Contato</a>
+    </li>
+    <li class="nav-item">
+    <a class="nav-link" href="login.html">Login</a>
+    </li>
     </ul>
-</nav>`
-
-element.innerHTML= navbar
-
-}
+    </div>
+    </div>
+    </nav>
+    `
+    element.innerHTML= navbar
+    }
+    function navBarComponentTemplate() {
+        const element = document.getElementById('navbar-component')
+        fetch("../components/nav-component.html")
+            .then((res) => res.text())
+            .then((nav) => {
+                element.innerHTML = nav
+            })
+            .catch((error) => {
+                console.error("Erro ao montar a navbar: ", error);
+            })
+    }
+    
