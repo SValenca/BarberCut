fc9a9c9a5e2e0d69514c643c4dd0f5dc2e2f8ce9
Programador Full Stack 2024.1 <16133232024.1@senacrjedu.local>
2024-10-31 21:53:35 -0300

    validate

--- js/validator.js
+++ js/validator.js
@@ -1,6 +1,7 @@
 validateName()
-validateRequired()
-
+//validateRequired()
+validateEmail()
+validateTel()
 function validateName() {
     const element = document.querySelector("#nameInput")
     const message = document.querySelector("#nameHelp")
@@ -8,7 +9,7 @@ function validateName() {
         const value = e.target.value
         console.log(value)
         if (value == '') {
-            message.innerHTML = '<span class="text-danger"> Valor invalido </span>'
+            message.innerHTML = '<span class="text-danger"> Nome invalido </span>'
         }
         else {
             message.innerHTML = ''
@@ -17,22 +18,36 @@ function validateName() {
 }
 
 function validateEmail() {
-    const element = document.querySelector("#nameEmail")
-    const message = document.querySelector("#nameEmail")
+    const element = document.querySelector("#emailInput")
+    const message = document.querySelector("#emailHelp")
     element.addEventListener('keyup', (e) => {
         const value = e.target.value
         console.log(value)
-        if (value == '') {
-            message.innerHTML = 'Valor invalido'
+        let error = ''
+        const regexEmail = (/^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{3}$/i);     
+        if (!regexEmail.test(value)) {
+            error= 'Email invalido'
         }
-        else {
-            message.innerHTML = ''
-        }
+            message.innerHTML = error ;
+        
     })
 }
 
-
-
+function validateTel(){
+    const element = document.querySelector('#telInput')
+    const message = document.querySelector('#telHelp')
+    element.addEventListener('keyup',(e) =>{
+        element.setAttribute('maxlenght','15')
+        const value = e.target.value
+        console.log(value)
+        let error = ''
+        const regexTel=(/^\([0-9]{2,3}\)\s[0-9]{5}-[0-9]{4}$/g);
+        if(!regexTel.test(value)){
+            error = 'Telefone invalido. Formato:"(999) 99999-9999"'
+        }
+    message.innerHTML = error;
+    })
+}
 
 function validateRequired() {
     const elements = document.querySelectorAll("[required]")
