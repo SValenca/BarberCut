da995bae61a1e49720164ebacbbbc21288cc0605
Programador Full Stack 2024.1 <16133232024.1@senacrjedu.local>
2024-11-05 21:43:47 -0300

    aula

--- js/validator.js
+++ js/validator.js
@@ -2,6 +2,9 @@ validateName()
 //validateRequired()
 validateEmail()
 validateTel()
+validateSenha()
+seletorDeDocumento()
+
 function validateName() {
     const element = document.querySelector("#nameInput")
     const message = document.querySelector("#nameHelp")
@@ -24,43 +27,58 @@ function validateEmail() {
         const value = e.target.value
         console.log(value)
         let error = ''
-        const regexEmail = (/^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{3}$/i);     
+        const regexEmail = (/^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{3}$/i);
         if (!regexEmail.test(value)) {
-            error= 'Email invalido'
+            error = 'Email invalido'
         }
-            message.innerHTML = error ;
-        
+        message.innerHTML = error;
+
     })
 }
 
-function validateTel(){
+function validateTel() {
     const element = document.querySelector('#telInput')
     const message = document.querySelector('#telHelp')
-    element.addEventListener('keyup',(e) =>{
-        element.setAttribute('maxlength','15')
+    element.addEventListener('keyup', (e) => {
+        element.setAttribute('maxlength', '15')
         const value = e.target.value
         console.log(value)
         let error = ''
-        const regexTel=(/^\([0-9]{2,3}\)\s[0-9]{5}-[0-9]{4}$/g);
-        if(!regexTel.test(value)){
+        const regexTel = (/^\([0-9]{2,3}\)\s[0-9]{5}-[0-9]{4}$/g);
+        if (!regexTel.test(value)) {
             error = 'Telefone invalido. Formato:"(999) 99999-9999"'
         }
-    message.innerHTML = error;
+        message.innerHTML = error;
     })
 }
-function changeDocumentType(){
-    const documentType = document.querySelector('#documentInput');
-    const documentLabel = document.querySelector('documentLabel');
+function validateSenha() {
+    const element = document.querySelector('#passwordInput')
+    const message = document.querySelector('#passwordHelp')
+
+    element.addEventListener('keyup', (e) => {
+        const value = e.target.value
+        console.log(value)
+        if (value.length < 8) {
+            message.innerHTML = 'Senha invalida! Minimo de 8 caracteres'
+        } else{
+            message.innerHTML.
+        }
+
+    })
+}
+function seletorDeDocumento() {
+    const documentType = document.querySelector('#documentInput')
+    const documentLabel = document.querySelector('documentLabel')
     const documentInput = document.querySelector('documentInput')
-    document.addEventListener('change',()=>{
-        if (documentType.value === 'cpf'){
-            documentLabel.textContent = 'CPF';
-            documentInput.placeholder = 'Digite seu CPF';
-        }else{
-            documentLabel.textContent = 'CNPJ';
+    document.addEventListener('change', () => {
+        if (documentType.value === 'cpf') {
+            documentLabel.textContent = 'CPF'
+            documentInput.placeholder = 'Digite seu CPF'
+        } else {
+            documentLabel.textContent = 'CNPJ'
             documentInput.placeholder = 'Digite seu CNPJ'
         }
-    });
+    })
 }
 
 
