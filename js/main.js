cc8b4f22e82b2625650cde7cf616da274b1243c8
Programador Full Stack 2024.1 <16133232024.1@senacrjedu.local>
2024-11-21 21:35:41 -0300

    DDD

--- js/main.js
+++ js/main.js
@@ -1,14 +1,7 @@
+import { navBarComponentTemplate } from "./../components/nav-components/nav-component.js"
 
-import { validateName, validateTel, validateEmail, validateSenha, confimacaoSenha } from './service/validator.js'
-import { navBarComponentTemplate } from "./../components/nav-component/nav-component.js"
-import{cardComponent}from "./../components/card-components/card-component.js"
 navBarComponentTemplate()
-cardComponent()
-validateName()
-validateTel()
-validateEmail()
-validateSenha()
-confimacaoSenha()
+
 //Função para pegar os elementos
 function getElement(elementSelector) {
     return document.querySelector(elementSelector)
